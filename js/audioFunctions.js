fdd9057dc9e62d749b0b4324e6f5b40829b2d71f
Usagination <tjddlf2645@gmail.com>
2018-01-05 12:04:36 +0900

    ㅇㅇ

--- js/audioFunctions.js
+++ js/audioFunctions.js
@@ -1,7 +1,14 @@
 
-var audio = new Audio('snd/FFXIV_Incoming_Tell_3.wav');
+var audio1 = new Audio('snd/FFXIV_Incoming_Tell_1.wav');
+var audio3 = new Audio('snd/FFXIV_Incoming_Tell_3.wav');
 
 function playSnd(type) {
-    if (type >= 18 && type <= 20)
-        audio.play();
+    switch (type) {
+        case 1:
+            audio1.play();
+            break;
+        case 3:
+            audio3.play();
+            break;
+    }
 }
