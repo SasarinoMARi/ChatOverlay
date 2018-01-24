bd27baa02ed0854baba5e17a3b43c949d47df26f
Usagination <tjddlf2645@gmail.com>
2018-01-24 14:33:51 +0900

    ' v '

--- js/translate.js
+++ js/translate.js
@@ -1,19 +1,23 @@
 function translate(text, callback) {
     if (localStorage.log_istranslate == 'true') {
-        var apiKey = 'fuck cloud';
+        var apiKey = localStorage.log_apikey;
         var url = "https://translation.googleapis.com/language/translate/v2?key=" + apiKey + "&q=" + text + "&target=ko";
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.onreadystatechange = function() { 
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            {
-                var r = JSON.parse(xmlHttp.responseText);
-                if (r.data.translations != undefined && r.data.translations.length > 0) {
-                    callback(r.data.translations[0].translatedText);
+            try {
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                {
+                    var r = JSON.parse(xmlHttp.responseText);
+                    if (r.data.translations != undefined && r.data.translations.length > 0) {
+                        callback(r.data.translations[0].translatedText);
+                    }
                 }
-            }
-            else {
-                var r = JSON.parse(xmlHttp.responseText);
-                console.log("status : " + xmlHttp.status + "    " + r.error.message);
+                else {
+                    var r = JSON.parse(xmlHttp.responseText);
+                    console.log("status : " + xmlHttp.status + "    " + r.error.message);
+                } 
+            } catch (error) {
+                
             }
         }
         xmlHttp.open("GET", url, true);
