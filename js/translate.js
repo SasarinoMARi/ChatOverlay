3951a4a6321bd80a2a1aa2064e50315eec2e0aed
Usagination <tjddlf2645@gmail.com>
2018-01-24 13:50:52 +0900

    번역 기능 추가

---
+++ js/translate.js
@@ -0,0 +1,20 @@
+function translate(text, callback) {
+    if (localStorage.log_istranslate == 'true') {
+        var apiKey = 'fuck cloud';
+        var url = "https://translation.googleapis.com/language/translate/v2?key=" + apiKey + "&q=" + text + "&target=ko";
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            {
+                var r = JSON.parse(xmlHttp.responseText);
+                if (r.data.translations != undefined && r.data.translations.length > 0) {
+                    callback(r.data.translations[0].translatedText);
+                }
+            }
+        }
+        xmlHttp.open("GET", url, true);
+        xmlHttp.send(null);
+        return xmlHttp.responseText;
+    }
+    else callback(text);
+}
