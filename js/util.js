582c653b33f0950367350e4367b8289ef21b42e3
Sasarino MARi <tjddlf2645@gmail.com>
2018-10-24 21:58:24 +0900

    Papago API Test 1

--- js/util.js
+++ js/util.js
@@ -36,11 +36,6 @@ const pFloat = function parseLocaledFloat(string) {
 }
 
 const getType = function getLogType(type) {
-
-  // 0 : talk
-  // 1 : battle
-  // 2 : event
-
   if (type < 40)
     return 0;
   else if (type < 50)
@@ -63,6 +58,17 @@ const getType = function getLogType(type) {
     return 1;
   return 0;
 };
+
+function filterLog(type) {
+  var isTalkLogEnabled = true;
+  var isBattleLogEnabled = false;
+  var isEventLogEnabled = true;
+
+  if (type == 0) return isTalkLogEnabled;
+  if (type == 1) return isBattleLogEnabled;
+  if (type == 2) return isEventLogEnabled;
+  return false;
+}
 
 function getTimeStampSpan(data) {
   var timestamp = new Date(data);
