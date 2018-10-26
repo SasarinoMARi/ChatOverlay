5d0403326e2e98dba217e351648d185683a40c88
Sasarino MARi <tjddlf2645@gmail.com>
2018-10-26 18:33:00 +0900

    프로젝트 포기 디스크립션 참조
    크로스 브라우저 문제는 OverlayPlugin.dll의 Security설정을 끄고 빌드하면 해결되는데 별개로 네이버 측에서 Javascript에서의 call 막아두었기 때문에 방법 없을 것으로 보임

--- js/translate.js
+++ js/translate.js
@@ -5,40 +5,9 @@ function translate(text, doTranslate, callback) {
         var papagoApiSecret = localStorage["papagoApiSecret"];
         var sourceLanguage = "ja";
         if (papagoApiKey != undefined && papagoApiSecret != undefined) {
-            if (papagoApiKey.length > 0 && papagoApiSecret.length > 0 ) {
-                var encodedText = encodeURIComponent(text);
-                console.log(papagoApiKey + " //// " + papagoApiSecret);
-                var url = "https://openapi.naver.com/v1/language/translate";
-                var body = "target=ko&source=" + sourceLanguage + "&text=" + encodedText
-                var xmlHttp = new XMLHttpRequest();
-                xmlHttp.onreadystatechange = function () {
-                    try {
-                        if (xmlHttp.readyState == 4) {
-
-                            if (xmlHttp.status == 200) {
-                                var r = JSON.parse(xmlHttp.responseText);
-                                console.log(r);
-                                if (r.message.result != undefined && r.message.result > 0) {
-                                    callback(r.message.result.replace("<", "&lt;").replace(">", "&gt;"));
-                                }
-                                else throw undefined;
-                            }
-                            else {
-                                var r = JSON.parse(xmlHttp.responseText);
-                                console.log("status : " + xmlHttp.status + "    " + r.error.message);
-                                throw undefined;
-                            }
-                        }
-                    } catch (error) {
-                        callback(text);
-                    }
-                }
-                xmlHttp.open("POST", url, true);
-                xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                xmlHttp.setRequestHeader("X-Naver-Client-Id", papagoApiKey);
-                xmlHttp.setRequestHeader("X-Naver-Client-Secret", papagoApiSecret);
-                xmlHttp.send(body);
-                return xmlHttp.responseText;
+            if (papagoApiKey.length > 0 && papagoApiSecret.length > 0) {
+                reqAjax(text, sourceLanguage, papagoApiKey, papagoApiSecret, callback)
+                //reqXML(text, sourceLanguage, papagoApiKey, papagoApiSecret, callback);
             }
         }
         else if (googleApiKey != undefined) {
@@ -74,4 +43,84 @@ function translate(text, doTranslate, callback) {
         }
     }
     callback(text);
+}
+
+function reqXML(text, sourceLanguage, papagoApiKey, papagoApiSecret, callback) {
+    var encodedText = encodeURIComponent(text);
+    var body = new FormData();
+    body.append('target', 'ko');
+    body.append('source', sourceLanguage);
+    body.append('text', encodedText);
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        try {
+            if (xmlHttp.readyState == 4) {
+                if (xmlHttp.status == 200) {
+                    var r = JSON.parse(xmlHttp.responseText);
+                    console.log(r);
+                    if (r.message.result != undefined && r.message.result > 0) {
+                        callback(r.message.result.replace("<", "&lt;").replace(">", "&gt;"));
+                    }
+                    else throw undefined;
+                }
+                else {
+                    console.log(xmlHttp);
+                    var r = JSON.parse(xmlHttp.responseText);
+                    console.log("status : " + xmlHttp.status + "    " + r.error.message);
+                    throw undefined;
+                }
+            }
+        } catch (error) {
+            console.log(error);
+            callback(text);
+        }
+    }
+    xmlHttp.open("GET", 'https://openapi.naver.com/v1/language/translate', true);
+    xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlHttp.setRequestHeader("X-Naver-Client-Id", papagoApiKey);
+    xmlHttp.setRequestHeader("X-Naver-Client-Secret", papagoApiSecret);
+    xmlHttp.send(body);
+    return xmlHttp.responseText;
+}
+
+function reqAjax(text, sourceLanguage, papagoApiKey, papagoApiSecret, callback) {
+    var encodedText = encodeURIComponent(text);
+    var targeturl = "https://openapi.naver.com/v1/language/translate";
+    $.ajax({
+        crossDomain: true,
+        context: this,
+        traditional: true,
+        url: targeturl,
+        type: 'POST',
+        method: 'POST',
+        dataType: "json",
+        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        async: true,
+        cache: true,
+        timeout: 0,
+        headers: {
+            "X-Naver-Client-Id": papagoApiKey,
+            "X-Naver-Client-Secret": papagoApiSecret,
+        },
+        data: {
+            "target": "ko",
+            "source": sourceLanguage,
+            "text": encodedText
+        },
+        beforeSend: function (jqXHR, settings) { 
+            jqXHR.setRequestHeader("x-requested-with", "*"); 
+            jqXHR.setRequestHeader("Access-Control-Allow-Origin", "*"); 
+        },
+        success: function (responseData, textStatus, jqXHR) {
+            console.log(responseData)
+            console.log(textStatus)
+            console.log(jqXHR)
+        },
+        error: function (responseData, textStatus, errorThrown) {
+            console.log(responseData);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
 }
